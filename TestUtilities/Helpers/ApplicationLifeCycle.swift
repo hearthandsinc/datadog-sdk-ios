271070073b4e19d167fd217a586d54f9edc1b9ca
Ganesh Jangir <ganesh.jangir@datadoghq.com>
2024-06-10 11:40:41 +0200

    RUM-3463 feat(watchdog-termination): track app state and detect watchdog terminations

---
+++ TestUtilities/Helpers/ApplicationLifeCycle.swift
@@ -0,0 +1,91 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-2020 Datadog, Inc.
+ */
+
+import XCTest
+import DatadogInternal
+
+public class ApplicationLifeCycle {
+    #if os(tvOS) || os(iOS)
+    let willEnterForegroundNotification = UIApplication.willEnterForegroundNotification
+    let didBecomeActiveNotification = UIApplication.didBecomeActiveNotification
+    let willResignActiveNotification = UIApplication.willResignActiveNotification
+    let didEnterBackgroundNotification = UIApplication.didEnterBackgroundNotification
+    let willTerminateNotification = UIApplication.willTerminateNotification
+    let didFinishLaunchingNotification = UIApplication.didFinishLaunchingNotification
+    #elseif os(macOS)
+    let didBecomeActiveNotification = NSApplication.didBecomeActiveNotification
+    let willResignActiveNotification = NSApplication.willResignActiveNotification
+    let willTerminateNotification = NSApplication.willTerminateNotification
+    let didFinishLaunchingNotification = NSApplication.didFinishLaunchingNotification
+    #endif
+
+    let notificationCenter: NotificationCenter
+
+    public init(notificationCenter: NotificationCenter = .default) {
+        self.notificationCenter = notificationCenter
+    }
+
+    public func goToForeground() {
+        willEnterForeground()
+        uiWindowDidBecomeVisible()
+        didBecomeActive()
+    }
+
+    public func goToBackground() {
+        willResignActive()
+        didEnterBackground()
+    }
+
+    public func terminateApp() {
+        willTerminate()
+    }
+
+    public func willEnterForeground() {
+        #if os(tvOS) || os(iOS)
+        post(name: willEnterForegroundNotification)
+        #endif
+    }
+
+    public func didBecomeActive() {
+        #if os(tvOS) || os(iOS) || os(macOS)
+        post(name: didBecomeActiveNotification)
+        #endif
+    }
+
+    public func willResignActive() {
+        #if os(tvOS) || os(iOS) || os(macOS)
+        post(name: willResignActiveNotification)
+        #endif
+    }
+
+    public func didEnterBackground() {
+        #if os(tvOS) || os(iOS)
+        post(name: didEnterBackgroundNotification)
+        #endif
+    }
+
+    public func willTerminate() {
+        #if os(tvOS) || os(iOS) || os(macOS)
+        post(name: willTerminateNotification)
+        #endif
+    }
+
+    public func didFinishLaunching() {
+        #if os(tvOS) || os(iOS) || os(macOS)
+        post(name: didFinishLaunchingNotification)
+        #endif
+    }
+
+    public func uiWindowDidBecomeVisible() {
+        #if os(tvOS) || os(iOS) || targetEnvironment(macCatalyst)
+        post(name: UIWindow.didBecomeVisibleNotification)
+        #endif
+    }
+
+    private func post(name: Notification.Name) {
+        notificationCenter.post(Notification(name: name))
+    }
+}
